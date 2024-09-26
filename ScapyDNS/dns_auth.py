815517f9bf58d21e2e656f6359ca8226337d5e0f
TochusC <205329624@qq.com>
2024-09-26 19:34:11 +0800

    Complete dns_auth.py, A simple python script to mimic auth-nameserver.

--- ScapyDNS/dns_auth.py
+++ ScapyDNS/dns_auth.py
@@ -1,36 +1,79 @@
 from scapy.all import *
-from scapy.layers.dns import DNS, DNSQR, DNSRR
+from scapy.layers.dns import DNS, DNSQR, DNSRR, DNSRRRSIG, DNSRRDNSKEY
 from scapy.layers.inet import IP, UDP
 import time
 
 # 配置DNS服务器的IP和端口
 IFACE_LAN = "eth0"
 PORT_OF_SERVER = "53"
+DOMAIN_NAME = ["www.keytrap.test.", "keytrap.test.", "ns1.keytrap.test."]
 
 # BPF过滤器
 BPF_FILTER = "udp port " + PORT_OF_SERVER 
 
+RRSIG={
+    "www.keytrap.test.": "u3i18XgPPsVN7bQlmWvSqPx0wtzNgsmEGW8FQ21Qiot67v4/MjkY8H27la+5eJIjo+eYIo1FpuUa2wxZ0eHA8dWck7ek0KFH/oibMSzvDWPkVBMwLFpjiOiiJVN5sOtw",
+    "keytrap.test.": "+XtSv7DeD9ASuujWbst5v73eq+pDGM0Qs27E9FahFTvJNVi6gOTQMoxnpCs0U8Way2Af8iP6OoKOfZY8RMrvsMI3k9JtGl1SKaztWA5vz9nVS/MhN1sOTIpkLU6Hvnfa",
+    "ns1.keytrap.test.": "/iNOASO1o0PtfxvlLIkrq/hb+jJqhbJeAstiNmjj/Ck7jYvBg+0xUL/rAaN9+RpA/vqa7uxX8zaI2xutux1avqQvHYngyU9ETjJNu6G6sLCqLTOrDPhrjbiV1OyVWdu2",
+}
+
+def handle_A_query(pkt):
+    qname = pkt[DNS].qd.qname.decode("utf-8")
+    if qname in DOMAIN_NAME:
+        spf_dns = DNS(id=pkt[DNS].id, qr=1, qd=pkt[DNS].qd,
+                        an=[
+                            DNSRR(rrname=qname, type=1, ttl=10, rdata="1.2.3.4"),
+                            DNSRRRSIG(rrname=qname, keytag=6350, signersname="keytrap.test", signature=RRSIG[qname]),
+                        ],
+                        ns=[
+                            DNSRR(rrname="keytrap.test.", type=2, ttl=60, rdata="ns1.keytrap.test."),
+                            DNSRRRSIG(rrname="keytrap.test.", keytag=6350, typecovered=2, signersname="keytrap.test", signature=RRSIG["keytrap.test."]),
+                        ],
+                        ar=[
+                            DNSRR(rrname="ns1.keytrap.test.", type=1, ttl=60, rdata="1.2.3.4"),
+                            DNSRRRSIG(rrname="ns1.keytrap.test.", keytag=6350, signersname="keytrap.test", signature=RRSIG["keytrap.test."]),
+                        ],
+                    )
+    else:
+        spf_dns = DNS(id=pkt[DNS].id, qr=1, qd=pkt[DNS].qd)
+    return spf_dns
+
+def handle_DNSKEY_query(pkt):
+    qname = pkt[DNS].qd.qname.decode("utf-8")
+    spf_dns = DNS(id=pkt[DNS].id, qr=1, qd=pkt[DNS].qd,
+                    an=[
+                        DNSRRDNSKEY(rrname="keytrap.test.", algorithm=14, flags=257, publickkey="MzJsFTtAo0j8qGpDIhEMnK4ImTyYwMwDPU5gt/FaXd6TOw6AvZDAj2hlhZvaxMXV6xCw1MU5iPv5ZQrb3NDLUU+TW07imJ5GD9YKi0Qiiypo+zhtL4aGaOG+870yHwuY"),
+                        DNSRRDNSKEY(rrname="keytrap.test.", algorithm=14, flags=256, publickkey="MjY4X0GT9jf00V9bZU7cMkceFGdUMgbeNK4afF6BB/VznyKXsZlTeX5IgrD/8BNWd1jMvvL5RlbBXbmy5022d34VqReK5IRA6WKxp9uzDBEpc6qoh2npdudDTsFMZKor"),
+                        DNSRRRSIG(rrname="keytrap.test.", keytag=6350, typecovered=48, signersname="keytrap.test.", signature="RV1t5PrsLi9tSUYq9ZNtFoXT8cmJYVUzOsn7xYPItKC7ky7Kpqiykk6HJIcnU4yyMbY2eLVfdjUZFqu0dKl5yQnJvuVswc6ZvbWUtPc1ElbETHKve+MqGZagRPfwulLT"),
+                        DNSRRRSIG(rrname="keytrap.test.", keytag=30130, typecovered=48, signersname="keytrap.test.", signature="n4xGxlkb2FsJ3/nSfPaznBYXM4lDbMXLcXvIuxMNoRwpkdoKgUDX1PcwxnPhVS8T8Pi9SFM7KdhEl6b0lj/wtZPJtEA1oncjoE5sC7atr66ncAIpt9XVkCufFpepA8P/"),
+                    ],
+                )
+    return spf_dns
+
 # 构造DNS响应
 def craft_dns_response(pkt):
-    spf_resp = IP(src=pkt[IP].dst, dst=pkt[IP].src) / UDP(sport=int(PORT_OF_SERVER), dport=pkt[UDP].sport)
-    spf_resp /= DNS(id=pkt[DNS].id, qr=1, qd=pkt[DNS].qd,
-                    ancount=1, an=DNSRR(rrname=pkt[DNS].qd.qname, type=1, ttl=10, rdata="1.2.3.4"),
-                    nscount=1, ns=DNSRR(rrname="example.com", type=2, ttl=60, rdata="ns.example.com"))
-    return spf_resp
+    reply_pkt = IP(src=pkt[IP].dst, dst=pkt[IP].src) / UDP(sport=int(PORT_OF_SERVER), dport=pkt[UDP].sport)
+    qtype = pkt[DNS].qd.qtype
+    
+    # 查询A记录
+    if qtype == 1:
+        reply_pkt /= handle_A_query(pkt)
+    # 查询DNSKEY记录
+    elif qtype == 48:
+        reply_pkt /= handle_DNSKEY_query(pkt)
+
+    return reply_pkt
 
 # 处理DNS查询请求
 def dns_response(pkt):
     try:
-        if DNS in pkt and pkt[DNS].qr == 0:  # 只处理查询请求
+        if DNS in pkt and pkt[DNS].qr == 0 and pkt[DNS].qd.qname.decode("utf-8") in DOMAIN_NAME:  # 只处理查询请求
             print("%s : fm %s:%d : %s A ?" % (
                 time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), pkt[IP].src, pkt[UDP].sport,
                 pkt[DNS].qd.qname.decode("utf-8")))
 
-            print(pkt)
-
             # 构建DNS响应
             resp = craft_dns_response(pkt)
-
             send(resp, iface=IFACE_LAN)
 
             print("%s : to %s:%d : %s A %s" % (
