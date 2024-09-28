b202d10bfb1959d98ef76032b10cde0ed968bff4
TochusC <205329624@qq.com>
2024-09-28 15:47:01 +0800

    Make dnssec functional.

--- ScapyDNS/dns_auth.py
+++ ScapyDNS/dns_auth.py
@@ -1,21 +1,33 @@
 from scapy.all import *
 from scapy.layers.dns import DNS, DNSQR, DNSRR, DNSRRRSIG, DNSRRDNSKEY
 from scapy.layers.inet import IP, UDP
-import time
+from datetime import datetime
+import base64
 
 # 配置DNS服务器的IP和端口
 IFACE_LAN = "eth0"
 PORT_OF_SERVER = "53"
-DOMAIN_NAME = ["www.keytrap.test.", "keytrap.test.", "ns1.keytrap.test."]
+DOMAIN_NAME = ["www.keytrap.test.", "keytrap.test.", "ns1.keytrap.test.", "_ta-75b2.keytrap.test."]
+GLOBAL_TTL = 86400
+INCEPTION = datetime.strptime("20240928033159", "%Y%m%d%H%M%S").timestamp() + 3600 * 8
+EXPIRATION = datetime.strptime("20241028033159", "%Y%m%d%H%M%S").timestamp() + 3600 * 8
 
-# BPF过滤器
 BPF_FILTER = "udp port " + PORT_OF_SERVER 
 
 RRSIG={
-    "www.keytrap.test.": "u3i18XgPPsVN7bQlmWvSqPx0wtzNgsmEGW8FQ21Qiot67v4/MjkY8H27la+5eJIjo+eYIo1FpuUa2wxZ0eHA8dWck7ek0KFH/oibMSzvDWPkVBMwLFpjiOiiJVN5sOtw",
-    "keytrap.test.": "+XtSv7DeD9ASuujWbst5v73eq+pDGM0Qs27E9FahFTvJNVi6gOTQMoxnpCs0U8Way2Af8iP6OoKOfZY8RMrvsMI3k9JtGl1SKaztWA5vz9nVS/MhN1sOTIpkLU6Hvnfa",
-    "ns1.keytrap.test.": "/iNOASO1o0PtfxvlLIkrq/hb+jJqhbJeAstiNmjj/Ck7jYvBg+0xUL/rAaN9+RpA/vqa7uxX8zaI2xutux1avqQvHYngyU9ETjJNu6G6sLCqLTOrDPhrjbiV1OyVWdu2",
+    "www.keytrap.test." : base64.b64decode("KgVnod8gSnLvzOYVSQGZafGTPRowJp/okxRFIYCCN3ez7fefodhefbPbPVQmFjRYBZ9jxjhcE00aOcLX1GGHYpxPSplasDvuHPwLNwJdb4qjIeYW1dmP+xoVMIcHjBZU"),
+    "keytrap.test."     : base64.b64decode("s0ZmWQHEJoZ51gzJMG0ZyGXWs5oRPnhi7+peKM3pKxTYyVHPHy1dV5ERkDncxQT8ayLrseDLSKeC+Sx46cbFigs+LHKBcdCcTg+W8oFn8L0GnzTtIuLaEf0Dvbn3d37/"),
+    "ns1.keytrap.test." : base64.b64decode("f+pnEtvtuV+/6gd1SmBttrPQ5IT6z4H90G26kCe03n3fnPlDzvTCorNCdrgy3P331b+r19+yn6Jc0uOx92Z7TnmfOXWDmLWR8uEGcZf9CWjbRYo8F+ckvoQIFH6JTPfF"),
+    "KSK": base64.b64decode("YAqwWzt0bBSz/eyX9oQmAcMEKKDD7J+OHymxi3hnzXjZ0fcIUMLrVGqznYdcbaHbW+X6RoERwCrXdOQru//lCU/UhcHjJj/bjalWYWxiQyGmzfb0bpZ/NGxngzrFaGW1"),
+    "ZSK": base64.b64decode("9wVtsq9/vpBWxGRjlvRDjHJyqsvrmsMwc3w1yaNNL4dRE3Od+wRSriJX6+utEU45jy1tZmvi7DfN4vF6VcN37ElQDREmOoD0wXDldIVR/o2Z40rWmheLBffdLCSfD8+n"),
 }
+
+DNSKEY={
+    "KSK": base64.b64decode("MzJsFTtAo0j8qGpDIhEMnK4ImTyYwMwDPU5gt/FaXd6TOw6AvZDAj2hl hZvaxMXV6xCw1MU5iPv5ZQrb3NDLUU+TW07imJ5GD9YKi0Qiiypo+zht L4aGaOG+870yHwuY"),
+    "ZSK": base64.b64decode("DcYreAh+USsK1mtv7bSR2iaQvShPUqCy7l/BRQXttAFupXp6pUaQZS+k ii+H2JJqd+rS4YgC3KCd/by8yQi5j+WSy2yRprSuFuDyqZMFnDT/Py+n GjmIa59+W1iMdEYb"),
+}
+
+
     
 
 # 构造DNS响应
@@ -25,28 +37,45 @@ def craft_dns_response(pkt, qname, qtype):
     # 查询A记录
     if qtype == 1:
         reply_pkt /= DNS(id=pkt[DNS].id, qr=1, aa=1, ad=1, qd=pkt[DNS].qd,
-                        ancount=1, nscount=1, arcount=1,
                         an=
-                            DNSRR(rrname=qname, type=1, ttl=60, rdata="1.2.3.4") / 
-                            DNSRR(rrname=qname, type=1, ttl=60, rdata="1.2.3.5"),
+                            DNSRR(rrname=qname, type=1, ttl=GLOBAL_TTL, rdata="124.222.27.40") /
+                            DNSRRRSIG(rrname=qname, labels=3, ttl=GLOBAL_TTL, typecovered=1, originalttl=GLOBAL_TTL, 
+                            expiration=EXPIRATION, inception=INCEPTION,keytag=6350, algorithm=14, 
+                            signersname="keytrap.test", signature=RRSIG["www.keytrap.test."]),
                         ns=
-                            DNSRR(rrname="keytrap.test.", type=2, ttl=60, rdata="ns1.keytrap.test.") /
-                            DNSRRRSIG(rrname="keytrap.test.", keytag=6350, typecovered=2, signersname="keytrap.test", signature=RRSIG["keytrap.test."]),
+                            DNSRR(rrname="keytrap.test.", type=2, ttl=GLOBAL_TTL, rdata="ns1.keytrap.test.") /
+                            DNSRRRSIG(rrname="keytrap.test.", labels=2, ttl=GLOBAL_TTL, typecovered=2, originalttl=GLOBAL_TTL,
+                             expiration=EXPIRATION, inception=INCEPTION, keytag=6350, algorithm=14, 
+                             signersname="keytrap.test", signature=RRSIG["keytrap.test."]),
                         ar=
-                            DNSRR(rrname="ns1.keytrap.test.", type=1, ttl=60, rdata="1.2.3.4") /
-                            DNSRRRSIG(rrname="ns1.keytrap.test.", keytag=6350, signersname="keytrap.test", signature=RRSIG["keytrap.test."]),
+                            DNSRR(rrname="ns1.keytrap.test.", type=1, ttl=GLOBAL_TTL, rdata="124.222.27.40") /
+                            DNSRRRSIG(rrname="ns1.keytrap.test.", labels=3, ttl=GLOBAL_TTL, typecovered=1, originalttl=GLOBAL_TTL, 
+                            expiration=EXPIRATION, inception=INCEPTION,keytag=6350, algorithm=14, 
+                            signersname="keytrap.test", signature=RRSIG["ns1.keytrap.test."]),
                     )
 
     # 查询DNSKEY记录
     elif qtype == 48:
         reply_pkt /= DNS(id=pkt[DNS].id, qr=1, aa=1, ad=1, qd=pkt[DNS].qd,
                     an=
-                        DNSRRDNSKEY(rrname="keytrap.test.", algorithm=14, flags=257, publickey="MzJsFTtAo0j8qGpDIhEMnK4ImTyYwMwDPU5gt/FaXd6TOw6AvZDAj2hlhZvaxMXV6xCw1MU5iPv5ZQrb3NDLUU+TW07imJ5GD9YKi0Qiiypo+zhtL4aGaOG+870yHwuY") /
-                        DNSRRDNSKEY(rrname="keytrap.test.", algorithm=14, flags=256, publickey="MjY4X0GT9jf00V9bZU7cMkceFGdUMgbeNK4afF6BB/VznyKXsZlTeX5IgrD/8BNWd1jMvvL5RlbBXbmy5022d34VqReK5IRA6WKxp9uzDBEpc6qoh2npdudDTsFMZKor") /
-                        DNSRRRSIG(rrname="keytrap.test.", keytag=6350, typecovered=48, signersname="keytrap.test.", signature="RV1t5PrsLi9tSUYq9ZNtFoXT8cmJYVUzOsn7xYPItKC7ky7Kpqiykk6HJIcnU4yyMbY2eLVfdjUZFqu0dKl5yQnJvuVswc6ZvbWUtPc1ElbETHKve+MqGZagRPfwulLT") /
-                        DNSRRRSIG(rrname="keytrap.test.", keytag=30130, typecovered=48, signersname="keytrap.test.", signature="n4xGxlkb2FsJ3/nSfPaznBYXM4lDbMXLcXvIuxMNoRwpkdoKgUDX1PcwxnPhVS8T8Pi9SFM7KdhEl6b0lj/wtZPJtEA1oncjoE5sC7atr66ncAIpt9XVkCufFpepA8P/"),
+                        DNSRRDNSKEY(rrname="keytrap.test.", ttl=GLOBAL_TTL, algorithm=14, flags=256, publickey=DNSKEY["ZSK"]) /
+                        DNSRRDNSKEY(rrname="keytrap.test.", ttl=GLOBAL_TTL, algorithm=14, flags=257, publickey=DNSKEY['KSK']) /
+                        DNSRRRSIG(rrname="keytrap.test.", labels=2, ttl=GLOBAL_TTL, typecovered=48, originalttl=GLOBAL_TTL,
+                        expiration=EXPIRATION, inception=INCEPTION, keytag=6350, algorithm=14, 
+                        signersname="keytrap.test.", signature=RRSIG["ZSK"]) /
+                        DNSRRRSIG(rrname="keytrap.test.", labels=2, ttl=GLOBAL_TTL, typecovered=48, originalttl=GLOBAL_TTL,
+                         expiration=EXPIRATION, inception=INCEPTION, keytag=30130, algorithm=14, 
+                         signersname="keytrap.test.", signature=RRSIG["KSK"]),
                 )
-
+    else:
+        reply_pkt /= DNS(id=pkt[DNS].id, qr=1, aa=1, ad=1, qd=pkt[DNS].qd, rcode=3,
+                         ns=
+                            DNSRR(rrname="keytrap.test.", type=2, ttl=GLOBAL_TTL, rdata="ns1.keytrap.test.") /
+                            DNSRRRSIG(rrname="keytrap.test.", labels=2, ttl=GLOBAL_TTL, typecovered=2, originalttl=GLOBAL_TTL, 
+                            expiration=EXPIRATION, inception=INCEPTION, keytag=6350, algorithm=14, 
+                            signersname="keytrap.test", signature=RRSIG["keytrap.test."])
+                        )
+        
     return reply_pkt
 
 # 处理DNS查询请求
@@ -56,6 +85,7 @@ def dns_response(pkt):
             return
 
         qname = pkt[DNS].qd.qname.decode("utf-8")
+        
         if qname not in DOMAIN_NAME:
             return
 
@@ -64,7 +94,7 @@ def dns_response(pkt):
 
         # 构建DNS响应
         resp = craft_dns_response(pkt, qname, qtype)
-        resp.show2()
+        # resp.show2()
         send(resp, iface=IFACE_LAN)
         print(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())} : to {pkt[IP].src}:{pkt[UDP].sport} : {qname}\n")
 
@@ -73,6 +103,3 @@ def dns_response(pkt):
 
 print("Start DNS Authority ...")
 sniff(filter=BPF_FILTER, prn=dns_response, iface=IFACE_LAN)
-
-# pkt = IP()/UDP()/DNS(rd=1, qd=DNSQR(qname="www.keytrap.test.", qtype=1))
-# craft_dns_response(pkt, "www.keytrap.test.", 1).show2()
