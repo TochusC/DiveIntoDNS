659446ae14ff71e867f2f2ad8bc70fecf44597ce
TochusC <205329624@qq.com>
2024-10-10 17:26:20 +0800

    Add CNAME Dokcerenv.

--- ScapyDNS/dns_auth.py
+++ ScapyDNS/dns_auth.py
@@ -21,14 +21,15 @@ BPF_FILTER = "udp port "  + str(PORT_OF_SERVER)
 
 # 每次签名都必须更新以下条目：INCEPTION, EXPIRATION, RRSIG
 GLOBAL_TTL = 86400
-INCEPTION = datetime.strptime("20240928101351", "%Y%m%d%H%M%S").timestamp() + 3600 * 8
-EXPIRATION = datetime.strptime("20241028101351", "%Y%m%d%H%M%S").timestamp() + 3600 * 8
+INCEPTION = datetime.strptime("20241007092918", "%Y%m%d%H%M%S").timestamp()
+EXPIRATION = datetime.strptime("20241106092918", "%Y%m%d%H%M%S").timestamp()
 RRSIG={
-    "www.keytrap.test." : base64.b64decode("KgVnod8gSnLvzOYVSQGZafGTPRowJp/okxRFIYCCN3ez7fefodhefbPbPVQmFjRYBZ9jxjhcE00aOcLX1GGHYpxPSplasDvuHPwLNwJdb4qjIeYW1dmP+xoVMIcHjBZU"),
-    "keytrap.test."     : base64.b64decode("s0ZmWQHEJoZ51gzJMG0ZyGXWs5oRPnhi7+peKM3pKxTYyVHPHy1dV5ERkDncxQT8ayLrseDLSKeC+Sx46cbFigs+LHKBcdCcTg+W8oFn8L0GnzTtIuLaEf0Dvbn3d37/"),
-    "ns1.keytrap.test." : base64.b64decode("f+pnEtvtuV+/6gd1SmBttrPQ5IT6z4H90G26kCe03n3fnPlDzvTCorNCdrgy3P331b+r19+yn6Jc0uOx92Z7TnmfOXWDmLWR8uEGcZf9CWjbRYo8F+ckvoQIFH6JTPfF"),
-    "KSK": base64.b64decode("YAqwWzt0bBSz/eyX9oQmAcMEKKDD7J+OHymxi3hnzXjZ0fcIUMLrVGqznYdcbaHbW+X6RoERwCrXdOQru//lCU/UhcHjJj/bjalWYWxiQyGmzfb0bpZ/NGxngzrFaGW1"),
-    "ZSK": base64.b64decode("9wVtsq9/vpBWxGRjlvRDjHJyqsvrmsMwc3w1yaNNL4dRE3Od+wRSriJX6+utEU45jy1tZmvi7DfN4vF6VcN37ElQDREmOoD0wXDldIVR/o2Z40rWmheLBffdLCSfD8+n"),
+    "www.keytrap.test." : base64.b64decode("XBHOlljD4lBFpWFmbe2sXNhEbnDaDPBMn2e1Pvw0AIt3NW9j92zP4GHGCVI3IS1Ea3uGq0nBcUHVet5hxsXmhkoeLzIuDtU8Va4O2LyFIdX5Km1lpdntwcr0OOIAmAN5"),
+    # RRSIG FOR NS
+    "keytrap.test."     : base64.b64decode("Kb3NEkEkeBuxcpIsRTrBx7QPRk+LQN75ExRKzyiCAkgpz4k7+0lCMKyRcEWGQ6Ow28IFK+FV+lkdRr4uVxsjpVmc5ZtTJjFEfNVv3UCyHufrX4lvneIUYfls6zTR5RBq"),
+    "ns1.keytrap.test." : base64.b64decode("14I5St5PVzzL4ucb2yTWq3BSOGAXfJCLH4/De2HMm3sKznFf+RCAtv3lF1fpJyLDsXi4db8cf96nn2RPhQNM9T6p3HFITSJs4P5GRTkGmJJlafMlCHIOo1Lfv1aLEf5h"),
+    "KSK": base64.b64decode("WjKQCEHx962QfA/xiJk7lL4ziiz/cv4i9LZt2KE6l/Sn8SJfPnEUaFC6qsTTXkEWpBQU+lioSwL6wzGKDnSYf+UBZq2CGdNrIkxKohKpDbKAqh2swL/DC8ljLpuNf3IF"),
+    "ZSK": base64.b64decode("ollmgXnsrtc2GVx5HbXV4XaWxygXX90FD0avArSIIPuGHqzzLJ6S9loVtwsdZ/CGdJo4nzIn8pTXQuboz2Hv8GRf7N2FFfyRcmz8wHQ+faLsrZL0MPNApzybfJJMept6"),
 }
 
 # DNS记录的RDATA
@@ -40,13 +41,16 @@ RRRDATA={
 
 # DNSKEY记录的公钥
 DNSKEY={
-    # Key Tag: 31510, Key Signing Key
+    # Key Tag: 30130, Key Signing Key
     "KSK": base64.b64decode("MzJsFTtAo0j8qGpDIhEMnK4ImTyYwMwDPU5gt/FaXd6TOw6AvZDAj2hl hZvaxMXV6xCw1MU5iPv5ZQrb3NDLUU+TW07imJ5GD9YKi0Qiiypo+zht L4aGaOG+870yHwuY"),
 
     # Key Tag: 6350, Zone Signing Key
     "ZSK": base64.b64decode("DcYreAh+USsK1mtv7bSR2iaQvShPUqCy7l/BRQXttAFupXp6pUaQZS+k ii+H2JJqd+rS4YgC3KCd/by8yQi5j+WSy2yRprSuFuDyqZMFnDT/Py+n GjmIa59+W1iMdEYb"),
 }
     
+
+def gen_random_rrsig(len=96):
+    return os.urandom(len)
 
 # 构造DNS响应
 def craft_dns_response(pkt, qname, qtype):
@@ -55,18 +59,21 @@ def craft_dns_response(pkt, qname, qtype):
     # 查询A记录
     if qtype == 1:
         reply_pkt /= DNS(id=pkt[DNS].id, qr=1, aa=1, ad=1, qd=pkt[DNS].qd,
+                        ancount=2,
                         an=
-                            DNSRR(rrname=qname, type=1, ttl=GLOBAL_TTL, rdata="124.222.27.40") /
+                            DNSRR(rrname=qname, type=1, ttl=GLOBAL_TTL, rdata=RRRDATA["www.keytrap.test."]) /
                             DNSRRRSIG(rrname=qname, labels=3, ttl=GLOBAL_TTL, typecovered=1, originalttl=GLOBAL_TTL, 
                             expiration=EXPIRATION, inception=INCEPTION,keytag=6350, algorithm=14, 
                             signersname="keytrap.test", signature=RRSIG["www.keytrap.test."]),
+                        nscount=2,
                         ns=
-                            DNSRR(rrname="keytrap.test.", type=2, ttl=GLOBAL_TTL, rdata="ns1.keytrap.test.") /
+                            DNSRR(rrname="keytrap.test.", type=2, ttl=GLOBAL_TTL, rdata=RRRDATA["keytrap.test."]) /
                             DNSRRRSIG(rrname="keytrap.test.", labels=2, ttl=GLOBAL_TTL, typecovered=2, originalttl=GLOBAL_TTL,
                              expiration=EXPIRATION, inception=INCEPTION, keytag=6350, algorithm=14, 
                              signersname="keytrap.test", signature=RRSIG["keytrap.test."]),
+                        arcount=2,
                         ar=
-                            DNSRR(rrname="ns1.keytrap.test.", type=1, ttl=GLOBAL_TTL, rdata="124.222.27.40") /
+                            DNSRR(rrname="ns1.keytrap.test.", type=1, ttl=GLOBAL_TTL, rdata=RRRDATA["ns1.keytrap.test."]) /
                             DNSRRRSIG(rrname="ns1.keytrap.test.", labels=3, ttl=GLOBAL_TTL, typecovered=1, originalttl=GLOBAL_TTL, 
                             expiration=EXPIRATION, inception=INCEPTION,keytag=6350, algorithm=14, 
                             signersname="keytrap.test", signature=RRSIG["ns1.keytrap.test."]),
@@ -75,6 +82,7 @@ def craft_dns_response(pkt, qname, qtype):
     # 查询DNSKEY记录
     elif qtype == 48:
         reply_pkt /= DNS(id=pkt[DNS].id, qr=1, aa=1, ad=1, qd=pkt[DNS].qd,
+                    ancount=4,
                     an=
                         DNSRRDNSKEY(rrname="keytrap.test.", ttl=GLOBAL_TTL, algorithm=14, flags=256, publickey=DNSKEY["ZSK"]) /
                         DNSRRDNSKEY(rrname="keytrap.test.", ttl=GLOBAL_TTL, algorithm=14, flags=257, publickey=DNSKEY['KSK']) /
@@ -87,8 +95,9 @@ def craft_dns_response(pkt, qname, qtype):
                 )
     else:
         reply_pkt /= DNS(id=pkt[DNS].id, qr=1, aa=1, ad=1, qd=pkt[DNS].qd, rcode=3,
+                         nscount=2,
                          ns=
-                            DNSRR(rrname="keytrap.test.", type=2, ttl=GLOBAL_TTL, rdata="ns1.keytrap.test.") /
+                            DNSRR(rrname="keytrap.test.", type=2, ttl=GLOBAL_TTL, rdata=RRRDATA["keytrap.test."]) /
                             DNSRRRSIG(rrname="keytrap.test.", labels=2, ttl=GLOBAL_TTL, typecovered=2, originalttl=GLOBAL_TTL, 
                             expiration=EXPIRATION, inception=INCEPTION, keytag=6350, algorithm=14, 
                             signersname="keytrap.test", signature=RRSIG["keytrap.test."])
