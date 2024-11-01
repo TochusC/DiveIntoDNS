451d12345aaf0010f5dbceba340c81850fdfac61
TochusC <205329624@qq.com>
2024-11-01 11:48:24 +0800

    增强DNSSEC Reponser，使其能够自动生成DS、生成DNSSKEY等，实现自动的信任链构建。

--- GoDNS/Responsers/DNSSEC/dnssec_responser.go
+++ GoDNS/Responsers/DNSSEC/dnssec_responser.go
@@ -8,12 +8,15 @@
  * @Modify Time        @Author     @Version    @Description
  * ----------------    --------    --------    -----------
  * 31/10/24 21:02      t0chus      0.0         None
+ * 01/11/24 11:29  	   t0chus      0.1         DNSSEC Delegation Implementation
  */
 
 package main
 
 import (
+	"crypto/sha1"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/tochusc/godns"
@@ -29,10 +32,10 @@ type DNSSECResponser struct {
 	ServerConf godns.DNSServerConfig
 	// 默认回复
 	DefaultResp godns.ResponseInfo
-	ZoneName    string
-	// DNSSEC 加密材料
-	DNSSECMaterial DNSSECMaterial
+	// 区域名与其相应 DNSSEC 材料的映射
+	DNSSECMap map[string]DNSSECMaterial
 }
+
 type DNSSECMaterial struct {
 	KSKTag        int
 	ZSKTag        int
@@ -41,17 +44,136 @@ type DNSSECMaterial struct {
 	DNSKEYRespSec []dns.DNSResourceRecord
 }
 
+func (d DNSSECResponser) CreateDNSSECMat(zoneName string) DNSSECMaterial {
+	pubKskRDATA, privKskBytes := dns.GenerateDNSKEY(dns.DNSSECAlgorithmECDSAP384SHA384, dns.DNSKEYFlagSecureEntryPoint)
+	pubZskRDATA, privZskBytes := dns.GenerateDNSKEY(dns.DNSSECAlgorithmECDSAP384SHA384, dns.DNSKEYFlagZoneKey)
+	pubZskRR := dns.DNSResourceRecord{
+		Name:  zoneName,
+		Type:  dns.DNSRRTypeDNSKEY,
+		Class: dns.DNSClassIN,
+		TTL:   86400,
+		RDLen: uint16(pubZskRDATA.Size()),
+		RData: &pubZskRDATA,
+	}
+	pubKskRR := dns.DNSResourceRecord{
+		Name:  zoneName,
+		Type:  dns.DNSRRTypeDNSKEY,
+		Class: dns.DNSClassIN,
+		TTL:   86400,
+		RDLen: uint16(pubKskRDATA.Size()),
+		RData: &pubKskRDATA,
+	}
+
+	// 生成密钥集签名
+	keySetSig := dns.GenerateRRSIG(
+		[]dns.DNSResourceRecord{
+			pubZskRR,
+			pubKskRR,
+		},
+		dns.DNSSECAlgorithmECDSAP384SHA384,
+		uint32(time.Now().UTC().Unix()+86400-3600),
+		uint32(time.Now().UTC().Unix()-3600),
+		uint16(dns.CalculateKeyTag(pubKskRDATA)),
+		zoneName,
+		privKskBytes,
+	)
+	sigRec := dns.DNSResourceRecord{
+		Name:  zoneName,
+		Type:  dns.DNSRRTypeRRSIG,
+		Class: dns.DNSClassIN,
+		TTL:   86400,
+		RDLen: uint16(keySetSig.Size()),
+		RData: &keySetSig,
+	}
+	// 生成 DNSSEC 材料
+	anSec := []dns.DNSResourceRecord{
+		pubZskRR,
+		pubKskRR,
+		sigRec,
+	}
+	return DNSSECMaterial{
+		KSKTag:        int(dns.CalculateKeyTag(pubKskRDATA)),
+		ZSKTag:        int(dns.CalculateKeyTag(pubZskRDATA)),
+		PrivateKSK:    privKskBytes,
+		PrivateZSK:    privZskBytes,
+		DNSKEYRespSec: anSec,
+	}
+}
+
+func (d DNSSECResponser) GenerateDS(zoneName string) dns.DNSRDATADS {
+	dnssecMat, ok := d.DNSSECMap[zoneName]
+	if !ok {
+		d.DNSSECMap[zoneName] = d.CreateDNSSECMat(zoneName)
+		dnssecMat = d.DNSSECMap[zoneName]
+	}
+	// digest = digest_algorithm( DNSKEY owner name | DNSKEY RDATA);
+	kskRData := dnssecMat.DNSKEYRespSec[1].RData.(*dns.DNSRDATADNSKEY)
+	plainText := make([]byte, dns.GetDomainNameWireLen(&zoneName)+kskRData.Size())
+	offser, err := dns.EncodeDomainNameToBuffer(&zoneName, plainText)
+	if err != nil {
+		panic(err)
+	}
+	kskRData.EncodeToBuffer(plainText[offser:])
+	// digest = SHA1(plainText)
+	digest := sha1.Sum(plainText)
+	sDigest := digest[:]
+
+	return dns.DNSRDATADS{
+		KeyTag:     uint16(dnssecMat.KSKTag),
+		Algorithm:  dns.DNSSECAlgorithmECDSAP384SHA384,
+		DigestType: dns.DNSSECDigestTypeSHA1,
+		Digest:     sDigest,
+	}
+}
+
 // Response 根据 DNS 查询信息生成 DNS 回复信息。
 func (d DNSSECResponser) Response(qInfo godns.QueryInfo) (godns.ResponseInfo, error) {
 	rInfo := d.InitResp(qInfo)
 
 	qType := qInfo.DNS.Question[0].Type
+	qName := qInfo.DNS.Question[0].Name
 	if qType == dns.DNSRRTypeDNSKEY {
-		rInfo.DNS.Answer = d.DNSSECMaterial.DNSKEYRespSec
+		dnssecMat, ok := d.DNSSECMap[qName]
+		if !ok {
+			d.DNSSECMap[qName] = d.CreateDNSSECMat(qName)
+			dnssecMat = d.DNSSECMap[qName]
+		}
+		rInfo.DNS.Answer = dnssecMat.DNSKEYRespSec
 		rInfo.DNS.Header.ANCount = uint16(len(rInfo.DNS.Answer))
 		rInfo.DNS.Header.RCode = dns.DNSResponseCodeNoErr
+	} else if qType == dns.DNSRRTypeDS {
+		ds := d.GenerateDS(qName)
+		qSignerName := qName[strings.Index(qName, ".")+1:]
+		rec := dns.DNSResourceRecord{
+			Name:  qName,
+			Type:  dns.DNSRRTypeDS,
+			Class: dns.DNSClassIN,
+			TTL:   86400,
+			RDLen: uint16(ds.Size()),
+			RData: &ds,
+		}
+		dnssecMat := d.DNSSECMap[qSignerName]
+		sig := dns.GenerateRRSIG(
+			[]dns.DNSResourceRecord{rec},
+			dns.DNSSECAlgorithmECDSAP384SHA384,
+			uint32(time.Now().UTC().Unix()+86400-3600),
+			uint32(time.Now().UTC().Unix()-3600),
+			uint16(dnssecMat.ZSKTag),
+			qSignerName,
+			dnssecMat.PrivateZSK,
+		)
+		sigRec := dns.DNSResourceRecord{
+			Name:  qName,
+			Type:  dns.DNSRRTypeRRSIG,
+			Class: dns.DNSClassIN,
+			TTL:   86400,
+			RDLen: uint16(sig.Size()),
+			RData: &sig,
+		}
+		rInfo.DNS.Answer = []dns.DNSResourceRecord{rec, sigRec}
+		rInfo.DNS.Header.ANCount = 2
+		rInfo.DNS.Header.RCode = dns.DNSResponseCodeNoErr
 	} else {
-		qName := qInfo.DNS.Question[0].Name
 		rec := dns.DNSResourceRecord{
 			Name:  qName,
 			Type:  dns.DNSRRTypeA,
@@ -60,14 +182,20 @@ func (d DNSSECResponser) Response(qInfo godns.QueryInfo) (godns.ResponseInfo, er
 			RDLen: 0,
 			RData: &dns.DNSRDATAA{Address: d.ServerConf.IP},
 		}
+		qSignerName := qName[strings.Index(qName, ".")+1:]
+		dnssecMat, ok := d.DNSSECMap[qSignerName]
+		if !ok {
+			d.DNSSECMap[qSignerName] = d.CreateDNSSECMat(qSignerName)
+			dnssecMat = d.DNSSECMap[qSignerName]
+		}
 		sig := dns.GenerateRRSIG(
 			[]dns.DNSResourceRecord{rec},
 			dns.DNSSECAlgorithmECDSAP384SHA384,
 			uint32(time.Now().UTC().Unix()+86400-3600),
 			uint32(time.Now().UTC().Unix()-3600),
-			uint16(d.DNSSECMaterial.ZSKTag),
-			d.ZoneName,
-			d.DNSSECMaterial.PrivateZSK,
+			uint16(dnssecMat.ZSKTag),
+			qSignerName,
+			dnssecMat.PrivateZSK,
 		)
 		sigRec := dns.DNSResourceRecord{
 			Name:  qName,
@@ -123,8 +251,6 @@ func main() {
 		Algorithm: dns.DNSSECAlgorithmECDSAP384SHA384,
 		PublicKey: pubKskBytes,
 	}
-	// 也可以使用 GenerateDNSKEY 生成KSK，
-	// 但随机生成的KSK无法保证解析器拥有能与该KSK匹配的 信任锚点。
 	// pubKskRDATA, privKskBytes := dns.GenerateDNSKEY(dns.DNSSECAlgorithmECDSAP384SHA384, dns.DNSKEYFlagSecureEntryPoint)
 
 	pubZskRDATA, privZskBytes := dns.GenerateDNSKEY(dns.DNSSECAlgorithmECDSAP384SHA384, dns.DNSKEYFlagZoneKey)
@@ -168,8 +294,8 @@ func main() {
 	}
 	// 生成 DNSSEC 材料
 	anSec := []dns.DNSResourceRecord{
-		pubKskRR,
 		pubZskRR,
+		pubKskRR,
 		sigRec,
 	}
 
@@ -214,13 +340,15 @@ func main() {
 						Additional: []dns.DNSResourceRecord{},
 					},
 				},
-				ZoneName: "test",
-				DNSSECMaterial: DNSSECMaterial{
-					KSKTag:        int(dns.CalculateKeyTag(pubKskRDATA)),
-					ZSKTag:        int(dns.CalculateKeyTag(pubZskRDATA)),
-					PrivateKSK:    privKskBytes,
-					PrivateZSK:    privZskBytes,
-					DNSKEYRespSec: anSec,
+				DNSSECMap: map[string]DNSSECMaterial{
+					// 信任锚点
+					"test": DNSSECMaterial{
+						KSKTag:        int(dns.CalculateKeyTag(pubKskRDATA)),
+						ZSKTag:        int(dns.CalculateKeyTag(pubZskRDATA)),
+						PrivateKSK:    privKskBytes,
+						PrivateZSK:    privZskBytes,
+						DNSKEYRespSec: anSec,
+					},
 				},
 			},
 		),
